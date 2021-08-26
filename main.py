399aef1ef662ebebfa9915b498b2b76d48494510
mizantoyyibun <untirta.mizan62@gmail.com>
2021-08-26 15:24:28 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+semua sintaksis dasar bahasa pemrograman terdiri dari 3 langkah ini :
+1. Sekuensial : langkah berurutan
+2. Percabangan: langkah melompat jika terpenuhi
+3. Perulangan : mengulang langkah yang sama berkali-kali sapai kondisi terpenuhi
+"""
+
+# Sekuensial
+print('ibu bilang: "pergi ke toko, belikan telor"')  # print itu perintah menulis teks
+print('Budi menjawab: "baik bu, apa yang harus saya beli?"')
+print('Ibu berkata: beli susu 1 Liter, jika ada telur beli 6"')
+print('Maka Budi berangkat ke toko')
+print('dan mulai berbelanja')
+
+# Percabangan
+milk_bottle_count = 17
+if milk_bottle_count > 0:
+    print('\nBudi mengecek harganya, ternyata uang cukup')
+    print('Budi membeli 1 botol susu')
+else:
+    print('\nBudi tidak jadi membeli 1 botol susu')
+
+print('Budi pulang kerumah dan menyerahkan susunya kepada ibu')
