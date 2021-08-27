33c8fb5b1cbf01f6f5f8b72046351de2bb43b9dd
mizantoyyibun <untirta.mizan62@gmail.com>
2021-08-27 14:21:21 +0700

    initial commit

--- Dasar Sintaksis Job Programmer.py
+++ Dasar Sintaksis Job Programmer.py
@@ -14,10 +14,17 @@ print('dan mulai berbelanja')
 
 # Percabangan
 milk_bottle_count = 17
+egg_count = 53
+print(f'\njumlah susu {milk_bottle_count} botol')
+print(f'jumlah telur {egg_count} butir')
 if milk_bottle_count > 0:
-    print('\nBudi mengecek harganya, ternyata uang cukup')
-    print('Budi membeli 1 botol susu')
+    print('\nBudi mengecek harganya, ternyata uangnya cukup')
+    print('maka Budi membeli 1 botol susu')
+    if egg_count > 0:
+        print("dan membeli 6 telur")
+    else:
+        print('Budi hanya membeli 1 botol susu')
 else:
     print('\nBudi tidak jadi membeli 1 botol susu')
 
-print('Budi pulang kerumah dan menyerahkan susunya kepada ibu')
+print('\nBudi pulang kerumah dan menyerahkan susunya kepada ibu')
